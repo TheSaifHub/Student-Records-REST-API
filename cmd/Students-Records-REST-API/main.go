df0e09c9811a9ccb59f8598614f6b5635a9d2d25
Saif Ibrahim <engineersaifibrahim@gmail.com>
2026-01-21 11:26:32 +0530

    config and main file

--- cmd/Students-Records-REST-API/main.go
+++ cmd/Students-Records-REST-API/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/TheSaifHub/Student-Records-REST-API/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello Saif!")
+	// load config
+	cfg := config.MustLoad()
+	// database setup
+
+	// setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to my first go project"))
+	})
+
+	// setup server
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	fmt.Printf("Server started %s", cfg.HTTPServer.Addr)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("Server failed to start.")
+		}
+	}()
+
 }
